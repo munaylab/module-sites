27ece9b3e788310068f9cb1c8e661e608557693e
Augusto Caligares <mcaligares@gmail.com>
2018-05-11 18:51:18 -0300

    refactorized bootstrap

--- grails-app/init/munaylab/sites/BootStrap.groovy
+++ grails-app/init/munaylab/sites/BootStrap.groovy
@@ -10,6 +10,9 @@ import org.munaylab.osc.Organizacion
 import org.munaylab.osc.EstadoOrganizacion
 import org.munaylab.osc.TipoOrganizacion
 import org.munaylab.osc.UserOrganizacion
+import org.munaylab.planificacion.Actividad
+import org.munaylab.planificacion.Proyecto
+import org.munaylab.planificacion.Programa
 import org.munaylab.security.Role
 import org.munaylab.security.UserRole
 
@@ -30,30 +33,36 @@ class BootStrap {
                 ?: new Role(authority: 'ROLE_OSC_ADMIN').save()
         def admin = TipoUsuario.findByNombre('ADMINISTRADOR')
                 ?: new TipoUsuario(nombre: 'ADMINISTRADOR').save()
-        def user = new User().with {
-            username    = 'mcaligares@gmail.com'
-            nombre      = 'Augusto'
-            apellido    = 'Caligares'
-            password    = 'Pass1234!'
-            it
+        def user = User.findByUsername('mcaligares@gmail.com')
+        if (!user) {
+            user = new User().with {
+                username    = 'mcaligares@gmail.com'
+                nombre      = 'Augusto'
+                apellido    = 'Caligares'
+                password    = 'Pass1234!'
+                it
+            }.save()
+            new UserRole(user: user, role: oscAdmin).save()
         }
-        def org = new Organizacion().with {
-            nombre      = 'MunayLab'
-            nombreURL   = 'munaylab'
-            tipo        = TipoOrganizacion.FUNDACION
-            estado      = EstadoOrganizacion.REGISTRADA
-            objeto      = 'Brindar herramientas innovadoras a las organizaciones de la sociedad civil.'
-            it
+        def org = Organizacion.findByNombre('MunayLab')
+        if (!org) {
+            org = new Organizacion().with {
+                nombre      = 'MunayLab'
+                nombreURL   = 'munaylab'
+                tipo        = TipoOrganizacion.FUNDACION
+                estado      = EstadoOrganizacion.REGISTRADA
+                objeto      = 'Brindar herramientas innovadoras a las organizaciones de la sociedad civil.'
+                it
+            }
+            org.addToAdmins(new UserOrganizacion(user: user, tipo: admin)).save()
         }
-        org.addToAdmins(new UserOrganizacion(user: user, tipo: admin)).save()
 
-        new UserRole(user: user, role: oscAdmin).save()
-
+        crearPlanificacion(org)
         agregarArticulos(org, user)
     }
 
     void agregarArticulos(Organizacion org, User user) {
-        def articulo = new Articulo().with {
+        def landing = new Articulo().with {
             autor           = user
             organizacion    = org
             imagen          = 'http://placehold.it/600x400'
@@ -67,7 +76,7 @@ class BootStrap {
         def principal = new Principal().with {
             titulo          = 'Titulo Principal'
             imagen          = 'http://www.csrindia.org/images/Banner-Social-Surfing.jpg'
-            contenido       = articulo
+            contenido       = landing
             organizacion    = org
             it
         }
@@ -79,101 +88,55 @@ class BootStrap {
         }
         principal.save(failOnError: true)
 
-        def mision = new Articulo().with {
-            autor           = user
-            organizacion    = org
-            imagen          = 'http://placehold.it/900x800'
-            publicado       = true
-            titulo          = 'Mision'
-            palabrasClaves  = 'mision'
-            descripcion     = 'descripcion mision'
-            contenido       = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-            it
-        }.save()
-        def nosotros = new Articulo().with {
-            autor           = user
-            organizacion    = org
-            imagen          = 'http://placehold.it/900x800'
-            publicado       = true
-            titulo          = 'Nosotros'
-            palabrasClaves  = 'nosotros'
-            descripcion     = 'descripcion nosotros'
-            contenido       = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-            it
-        }.save()
-        def programas = new Articulo().with {
-            autor           = user
-            organizacion    = org
-            imagen          = 'http://placehold.it/900x800'
-            publicado       = true
-            titulo          = 'Programas'
-            palabrasClaves  = 'programas'
-            descripcion     = 'descripcion programas'
-            contenido       = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-            it
-        }.save()
-        def contacto = new Articulo().with {
-            autor           = user
-            organizacion    = org
-            imagen          = 'http://placehold.it/900x800'
-            publicado       = true
-            titulo          = 'Contacto'
-            palabrasClaves  = 'contacto'
-            descripcion     = 'descripcion contacto'
-            contenido       = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-            it
-        }.save()
-        def contribuir = new Articulo().with {
-            autor           = user
-            organizacion    = org
-            imagen          = 'http://placehold.it/900x800'
-            publicado       = true
-            titulo          = 'Contribuir'
-            palabrasClaves  = 'contribuir'
-            descripcion     = 'descripcion contribuir'
-            contenido       = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-            it
-        }.save()
+        ['Mision', 'Nosotros', 'Programas', 'Contribuir', 'Contacto'].eachWithIndex { nombre, i ->
+            def articulo = new Articulo().with {
+                titulo          = nombre
+                contenido       = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+                imagen          = 'http://placehold.it/900x800'
+                palabrasClaves  = nombre.toLowerCase()
+                descripcion     = "descripcion ${nombre.toLowerCase()}"
+                publicado       = true
+                autor           = user
+                organizacion    = org
+                it
+            }.save()
+            def cabecera = new Cabecera().with {
+                organizacion    = org
+                contenido       = articulo
+                titulo          = articulo.titulo
+                nombre          = articulo.titulo.toLowerCase()
+                link            = nombre == 'Contacto' ? 'mailto:contacto@munaylab.org' : null
+                prioridad       = i
+                it
+            }.save()
+        }
+    }
 
-        new Cabecera().with {
+    void crearPlanificacion(Organizacion org) {
+        def programa = new Programa().with {
+            nombre          = 'Innovaciones Sociales'
+            imagen          = 'programa/innovacion'
+            descripcion     = 'Brindar innovaciones a las osc.'
+            publicado       = true
             organizacion    = org
-            contenido       = mision
-            titulo          = mision.titulo
-            nombre          = mision.titulo.toLowerCase()
-            prioridad       = 0
             it
         }.save()
-        new Cabecera().with {
+        def proyecto = new Proyecto().with {
+            nombre          = 'Taller de Innovaciones'
+            imagen          = 'proyecto/innovacion'
+            descripcion     = 'Taller donde se exponen innovaciones para la sociedad.'
+            publicado       = true
             organizacion    = org
-            contenido       = nosotros
-            titulo          = nosotros.titulo
-            nombre          = nosotros.titulo.toLowerCase()
-            prioridad       = 1
+            programa        = programa
             it
         }.save()
-        new Cabecera().with {
+        def actividad = new Actividad().with {
+            nombre          = 'Presentacion Innovacion'
+            imagen          = 'actividad/innovacion'
+            descripcion     = 'Presentacion de innovaciones realizadas en el taller.'
+            publicado       = true
             organizacion    = org
-            contenido       = programas
-            titulo          = programas.titulo
-            nombre          = programas.titulo.toLowerCase()
-            prioridad       = 2
-            it
-        }.save()
-        new Cabecera().with {
-            organizacion    = org
-            contenido       = contacto
-            titulo          = contacto.titulo
-            nombre          = contacto.titulo.toLowerCase()
-            link            = 'mailto:contacto@munaylab.org'
-            prioridad       = 3
-            it
-        }.save()
-        new Cabecera().with {
-            organizacion    = org
-            contenido       = contribuir
-            titulo          = contribuir.titulo
-            nombre          = contribuir.titulo.toLowerCase()
-            prioridad       = 4
+            proyecto        = proyecto
             it
         }.save()
     }
