7f2c9fc5d8031400c9562f43f4b680c008f2d032
Augusto Caligares <mcaligares@gmail.com>
2018-06-28 22:14:36 -0300

    agregado test para generar menu en taglib

--- src/test/groovy/munaylab/sites/SitesTagLibSpec.groovy
+++ src/test/groovy/munaylab/sites/SitesTagLibSpec.groovy
@@ -1,9 +1,28 @@
 package munaylab.sites
 
+import org.munaylab.ContenidoService
+import org.munaylab.contenido.Menu
+import org.munaylab.osc.Organizacion
+
 import grails.testing.web.taglib.TagLibUnitTest
 import spock.lang.Specification
 
 class SitesTagLibSpec extends Specification implements TagLibUnitTest<SitesTagLib> {
+
+    def setup() {
+        tagLib.contenidoService = Mock(ContenidoService)
+    }
+
+    void "menu de organizacion"() {
+        given:
+        def menues = [new Menu(nombre: 'menu1'), new Menu(nombre: 'menu2')]
+        1 * tagLib.contenidoService.getMenuDeOrganizacion(_) >> {menues}
+        when:
+        def output = tagLib.menu(org: new Organizacion())
+        then:
+        output.contains('menu1')
+        output.contains('menu2')
+    }
 
     void "articulo link"() {
         when:
